5d02dd0a321a17506f877e1ad63f09c74d855fa1
Janaina Front-end <janaina_2012silva@hotmail.com>
2025-02-06 20:42:10 -0300

    filtro

--- app/index.js
+++ app/index.js
@@ -1,13 +1,14 @@
-const api_url = 'https://api.disneyapi.dev/character?pageSize=300';
-const loading = document.getElementById('loading')
+const api_url = 'https://api.disneyapi.dev/character?pageSize=40';
+const loading = document.getElementById('loading');
+const titlecat = document.getElementById('title-category')
 let disney = []
 
 async function fetchData() {
     toogleLoading();
-    try{
+    try {
         const response = await fetch(api_url);
 
-        if(!response.ok){
+        if (!response.ok) {
             throw new Error('Erro ao acessar a API');
         }
 
@@ -17,38 +18,89 @@ async function fetchData() {
         console.log(data);
         displayMovies(disney);
 
-    } catch (error){
+    } catch (error) {
         console.error('Erro:', error)
     }
 }
 
 fetchData();
 
-function displayMovies(movies){
+
+
+function displayMovies(movies) {
     const container = document.getElementById('category-body');
     container.innerHTML = '';
+    let content = '';
+    let card = ''
 
-    movies.forEach(item =>{
-        let card = `<div class="card" id="card">
+    movies.forEach(item => {
+        card = `<div class="card" id="card">
                         <figure class="card__figure">
-                            <img src="${item.imageUrl}" alt="" />
+                            <img src="${item.imageUrl}" alt="${item.name}" />
                         </figure>
                         <h4 class="card__title">${item.name}</h4>
-                        <span class="card__date"> ${new Date(item.createdAt).getFullYear()}</span>
+                        <span class="card__date">${formatCalendar(item.updatedAt)}</span>
                     </div>`;
 
-        container.innerHTML += card;
-
+        content += card;
     });
 
-    toogleLoading();
+    setTimeout(()=>{
+    container.innerHTML = content;
+
+ 
+        toogleLoading();
+    }, 700)
+
 }
 
-function toogleLoading(){
+function listTodos() {
+    titlecat.innerText = 'Todos personagens'
+    toogleLoading();
+    displayMovies(disney);
+   
+}
 
-    if(loading.style.display == "block" || loading.style.display == " "){
-          loading.style.display = 'none';
-    }else{
+function listFilmes(){
+    titlecat.innerText = 'Personagens de Filmes'
+   const films = disney.filter(item => item.films.length > 1 )
+   toogleLoading();
+   displayMovies(films);
+}
+
+ function listTv(){
+    titlecat.innerText = 'Personagens de Tv'
+    const Tv = disney.filter(item => item.tvShows.length > 1 )
+    toogleLoading();
+    displayMovies(Tv);
+ }
+
+ function listJogos(){
+    titlecat.innerText = 'Personagens de Jogos'
+    const jogos = disney.filter(item => item.videoGames.length > 1 )
+    toogleLoading();
+    displayMovies(jogos);
+ }
+
+
+function formatCalendar(item) {
+    let date = new Date(item);
+    let year = date.getFullYear();
+    let month = formatMonth(date.getMonth())
+    return year;
+}
+
+function formatMonth(month) {
+    const newmonth = ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez']
+
+    return newmonth[month]
+}
+
+function toogleLoading() {
+
+    if (loading.style.display == "block" || loading.style.display == " ") {
+        loading.style.display = 'none';
+    } else {
         loading.style.display = 'block'
     }
 }
